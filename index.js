d66ae2407f901a50c867df4b26f7734dee124760
ColdDionysus <gautampukar02@gmail.com>
2023-08-01 18:11:08 +0545

    Variable Defination

---
+++ index.js
@@ -0,0 +1,21 @@
+//variable (var, let , and const)
+//ES5
+
+var person = "ram"
+person = "Shyam"
+
+console.log(person)
+
+
+//es6 
+
+
+const person1 = "ram"
+console.log(person1)
+
+
+//let
+
+let person2 = "MongoDB "
+
+console.log(person2)
